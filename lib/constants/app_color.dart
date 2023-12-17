7fa42076c20e083c51a57c7406ae23853aa34ab2
ensarsnr <dev.anise@outlook.com>
2023-12-17 23:25:27 +0300

    Created constan colors

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const ALGA_MOSS = Color(0xffb88AB8E);
+  static const ZANAH = Color(0xffbAFC8AD);
+  static const EDELWEISS = Color(0xffbEEE7DA);
+  static const NANO_WHITE = Color(0xffbF2F1EB);
+  static const GREEN_MYTH = Color(0xffbC3E2C2);
+}
