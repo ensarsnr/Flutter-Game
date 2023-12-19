f29da0ab5697d33965af32d93dd5fb59e2f6a735
ensarsnr <dev.anise@outlook.com>
2023-12-19 19:09:40 +0300

    Added new colors

--- lib/constants/app_color.dart
+++ lib/constants/app_color.dart
@@ -5,5 +5,9 @@ class ColorConstants {
   static const ZANAH = Color(0xffbAFC8AD);
   static const EDELWEISS = Color(0xffbEEE7DA);
   static const NANO_WHITE = Color(0xffbF2F1EB);
-  static const GREEN_MYTH = Color(0xffbC3E2C2);
+  static const GREEN_MYTH = Color(0xffb7E30E1);
+  static const COTTON_BALL = Color(0xffbF3F8FF);
+  static const HIBISCUS_POP = Color(0xffbE26EE5);
+  static const THICK_PURPLE = Color(0xffb7E30E1);
+  static const PITCH_VIOLET = Color(0xffb49108B);
 }
