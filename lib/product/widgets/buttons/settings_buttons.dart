363e954ffc6cbaacf3f28ea979ef7c59650593e1
ensarsnr <dev.anise@outlook.com>
2023-12-21 23:48:56 +0300

    Created settings button widget

---
+++ lib/product/widgets/buttons/settings_buttons.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SettingButton extends StatelessWidget {
+  SettingButton(
+      {super.key,
+      required this.child,
+      required this.onPressed,
+      required this.color});
+
+  final Widget child;
+  final VoidCallback onPressed;
+  Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: color,
+          border: Border.all(
+              color: const Color.fromARGB(255, 252, 162, 192), width: 2),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: child,
+      ),
+    );
+    ;
+  }
+}
