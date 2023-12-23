60b6bc06e4adf59c349b7264ab316e3b41fc6513
ensarsnr <dev.anise@outlook.com>
2023-12-24 00:51:19 +0300

    Added data to Provider

--- lib/feature/view/settings.dart
+++ lib/feature/view/settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_game/product/widgets/buttons/settings_buttons.dart';
 import 'package:flutter_game/product/widgets/container/settings_container.dart';
+import 'package:flutter_game/provider/settings_provider.dart';
+import 'package:provider/provider.dart';
 
 class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
@@ -13,11 +15,26 @@ class _SettingsViewState extends State<SettingsView> {
   Color musicOff = Colors.black.withOpacity(0);
   Color vibOff = Colors.black.withOpacity(0);
 
+  // ses ve titreşim ayarı
   bool isVib = true;
   bool isMusic = true;
 
+  // Kullanıcıdan takım adı al
+  TextEditingController teamOneController =
+      TextEditingController(text: "Takım 1");
+
+  TextEditingController teamTwoController =
+      TextEditingController(text: "Takım 2");
+
+  // Puan, saniye ve hak işlemleri
+  int point = 20;
+  int second = 50;
+  int health = 3;
+
   @override
   Widget build(BuildContext context) {
+    SettingsProvider settingsProvider =
+        Provider.of<SettingsProvider>(context, listen: false);
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
@@ -45,7 +62,7 @@ class _SettingsViewState extends State<SettingsView> {
               padding: const EdgeInsets.only(left: 10, right: 10),
               alignment: Alignment.centerLeft,
               child: TextFormField(
-                initialValue: "Takım 1",
+                controller: teamOneController,
                 decoration: const InputDecoration(
                   border: InputBorder.none,
                 ),
@@ -62,8 +79,10 @@ class _SettingsViewState extends State<SettingsView> {
               padding: const EdgeInsets.only(left: 10, right: 10),
               alignment: Alignment.centerLeft,
               child: TextFormField(
-                initialValue: "Takım 2",
-                decoration: const InputDecoration(border: InputBorder.none),
+                controller: teamTwoController,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
               ),
             ),
           ),
@@ -119,29 +138,44 @@ class _SettingsViewState extends State<SettingsView> {
             screenHeight: screenHeight,
             screenWidth: screenWidth,
             child: Container(
-              padding: EdgeInsets.only(left: 10, right: 10),
+              padding: const EdgeInsets.only(left: 10, right: 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   SettingButton(
                     child: Icon(Icons.add),
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        point = point + 5;
+                        if (point > 100) {
+                          point = 100;
+                        }
+                      });
+                    },
                     color: Colors.black.withOpacity(0),
                   ),
                   Text(
-                    "20 puan",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    '${point.toString()} puan',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 20),
                   ),
                   SettingButton(
                     child: Icon(Icons.remove),
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        point = point - 5;
+                        if (point < 0) {
+                          point = 0;
+                        }
+                      });
+                    },
                     color: Colors.black.withOpacity(0),
                   ),
                 ],
               ),
             ),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           SettingsContainer(
             screenHeight: screenHeight,
             screenWidth: screenWidth,
@@ -152,50 +186,79 @@ class _SettingsViewState extends State<SettingsView> {
                 children: [
                   SettingButton(
                     child: Icon(Icons.add),
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        second = second + 10;
+                        if (second > 600) {
+                          second = 600;
+                        }
+                      });
+                    },
                     color: Colors.black.withOpacity(0),
                   ),
                   Text(
-                    "50 saniye",
+                    '${second.toString()} saniye',
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   ),
                   SettingButton(
                     child: Icon(Icons.remove),
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        second = second - 10;
+                        if (second < 10) {
+                          second = 10;
+                        }
+                      });
+                    },
                     color: Colors.black.withOpacity(0),
                   ),
                 ],
               ),
             ),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           SettingsContainer(
             screenHeight: screenHeight,
             screenWidth: screenWidth,
             child: Container(
-              padding: EdgeInsets.only(left: 10, right: 10),
+              padding: const EdgeInsets.only(left: 10, right: 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   SettingButton(
                     child: Icon(Icons.add),
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        health++;
+                        if (health > 5) {
+                          health = 5;
+                        }
+                      });
+                    },
                     color: Colors.black.withOpacity(0),
                   ),
                   Text(
-                    "3 hak",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    '${health.toString()} hak',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 20),
                   ),
                   SettingButton(
                     child: Icon(Icons.remove),
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        health--;
+                        if (health < 0) {
+                          health = 0;
+                        }
+                      });
+                    },
                     color: Colors.black.withOpacity(0),
                   ),
                 ],
               ),
             ),
           ),
-          SizedBox(height: 30),
+          const SizedBox(height: 30),
           ElevatedButton(
             child: Text(
               'Kaydet',
@@ -205,13 +268,18 @@ class _SettingsViewState extends State<SettingsView> {
                   fontSize: 20),
             ),
             style: ElevatedButton.styleFrom(
-              padding:
-                  EdgeInsets.only(left: 100, right: 100, top: 15, bottom: 15),
+              padding: const EdgeInsets.only(
+                  left: 100, right: 100, top: 15, bottom: 15),
               primary: Colors.blue,
-              shape: RoundedRectangleBorder(
+              shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(10))),
             ),
-            onPressed: () {},
+            onPressed: () {
+              settingsProvider.setTeam1(teamOneController.text);
+              settingsProvider.setTeam2(teamTwoController.text);
+              settingsProvider.setLives(health);
+              settingsProvider.setSecond(second);
+            },
           ),
         ],
       ),
