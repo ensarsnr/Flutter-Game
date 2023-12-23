7b60337b04548028961e102d8d1eefc6263fba9b
ensarsnr <dev.anise@outlook.com>
2023-12-23 15:39:00 +0300

    Added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_game/feature/view/home.dart';
+import 'package:flutter_game/provider/settings_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SettingsProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
