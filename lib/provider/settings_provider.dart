c619cfd4cce2b18c6b02a7fba9fe619069338880
ensarsnr <dev.anise@outlook.com>
2023-12-23 15:39:42 +0300

    Created settings provider

---
+++ lib/provider/settings_provider.dart
@@ -0,0 +1,53 @@
+import 'dart:collection';
+import 'package:flutter/material.dart';
+import '../model/settings_model.dart';
+
+class SettingsProvider extends ChangeNotifier {
+  List<Settings> _decisions = [];
+
+  UnmodifiableListView<Settings> get allDecisions =>
+      UnmodifiableListView(_decisions);
+
+  String? team1;
+  String? team2;
+  int? lives;
+  bool? musicOff;
+  int? point;
+  int? second;
+  bool? vibOff;
+
+  void setTeam1(String value) {
+    team1 = value;
+    notifyListeners();
+  }
+
+  void setTeam2(String value) {
+    team2 = value;
+    notifyListeners();
+  }
+
+  void setLives(int value) {
+    lives = value;
+    notifyListeners();
+  }
+
+  void setMusicOff(bool value) {
+    musicOff = value;
+    notifyListeners();
+  }
+
+  void setPoint(int value) {
+    point = value;
+    notifyListeners();
+  }
+
+  void setSecond(int value) {
+    second = value;
+    notifyListeners();
+  }
+
+  void setVibOff(bool value) {
+    vibOff = value;
+    notifyListeners();
+  }
+}
