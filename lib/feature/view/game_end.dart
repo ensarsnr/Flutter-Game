565a6008685cbd8d4ac969bb94ce197dd79bd1b3
ensarsnr <dev.anise@outlook.com>
2023-12-24 19:16:04 +0300

    Created view

---
+++ lib/feature/view/game_end.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // bakarız
+    return Container(
+      child: Icon(Icons.emoji_events_sharp),
+    );
+  }
+}
