0a1e051358411a48221ee871727026063d568c2b
ensarsnr <dev.anise@outlook.com>
2023-12-27 00:23:24 +0300

    Created words model

---
+++ lib/model/words/words_list.dart
@@ -0,0 +1,8 @@
+import './list_data.dart';
+
+var tabooWords = [
+  ListData("Tasarruf etmek", ["Para", "Zaman", "Daha sonra", "Banka"]),
+  ListData("Ödemek", ["Para", "Fiyat", "Satın almak", "Satmak"]),
+  ListData("Kafa", ["Üst", "Yüz", "Yuvarlak", "Saç"]),
+  ListData("Kelime", ["Yazmak", "Cümle", "Harf", "Konuşmak"])
+];
