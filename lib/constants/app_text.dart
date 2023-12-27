5f9dbdceb302d696a9a317cb651fa08ab2ff735e
ensarsnr <dev.anise@outlook.com>
2023-12-27 23:53:29 +0300

    Added new text

--- lib/constants/app_text.dart
+++ lib/constants/app_text.dart
@@ -1,7 +1,11 @@
 class TextConstants {
   // Home
-  static const appTitle = "TABU";
+  static const appTitle = "DABUU";
   static const playButton = "Oyna";
   static const settingsButton = "Ayarlar";
   static const exitButton = "Çıkış";
+  static const attention = "Dikkat";
+  static const info = "Oyundan ayrılmak istediğine emin misin?";
+  static const confirmButton = "Evet";
+  static const draw = "Berabere";
 }
