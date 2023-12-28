1cb67372713b695f3ab0015dba8f19754d39280f
ensarsnr <dev.anise@outlook.com>
2023-12-28 16:11:17 +0300

    Design game end view

--- lib/feature/view/game_end.dart
+++ lib/feature/view/game_end.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class EndGame extends StatelessWidget {
+  final String winnerTeam;
+  const EndGame({super.key, required this.winnerTeam});
 
   @override
   Widget build(BuildContext context) {
     // bakarız
-    return Container(
-      child: Icon(Icons.emoji_events_sharp),
+    return Column(
+      children: [
+        const Icon(
+          Icons.emoji_events_sharp,
+          size: 200,
+          color: Color.fromARGB(255, 240, 218, 20),
+        ),
+        Text(
+          winnerTeam,
+          style: const TextStyle(
+              color: Color.fromARGB(255, 74, 74, 74),
+              fontSize: 50,
+              fontWeight: FontWeight.bold),
+        ),
+      ],
     );
   }
 }
