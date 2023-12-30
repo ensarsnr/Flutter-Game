6469642c698c4c53ae1d9c401402ec5a89a4fa4d
ensarsnr <dev.anise@outlook.com>
2023-12-30 15:15:03 +0300

    Added button for exit the game

--- lib/product/widgets/container/end_bottom.dart
+++ lib/product/widgets/container/end_bottom.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_game/feature/view/home.dart';
-import 'package:flutter_game/product/widgets/buttons/game_buttons.dart';
+import 'package:flutter_game/constants/app_color.dart';
+import 'package:flutter_game/constants/app_text.dart';
+import 'package:flutter_game/feature/view/game.dart';
 
 class EndBottom extends StatelessWidget {
   final double screenHeight;
@@ -9,24 +10,60 @@ class EndBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: screenHeight * 0.13,
-      decoration: const BoxDecoration(
+        height: screenHeight * 0.13,
+        decoration: const BoxDecoration(
           color: Color.fromRGBO(126, 48, 225, 0.984),
           borderRadius: BorderRadius.only(
-              topRight: Radius.circular(30), topLeft: Radius.circular(30))),
-      child: Center(
-        child: GameButtons(
-            padding: const EdgeInsets.only(top: 100),
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(30),
+          ),
+        ),
+        child: Container(
+          alignment: Alignment.center,
+          child: ElevatedButton(
             onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => const HomeView()));
+              showDialog(
+                context: context,
+                builder: (BuildContext contex) {
+                  return AlertDialog(
+                    title: const Text(TextConstants.attention),
+                    actions: [
+                      const Text(TextConstants.info),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: ColorConstants.ALERT_BUTTON_COLOR,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () => goToHome(contex),
+                        child: const Text(
+                          TextConstants.continueText,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
             },
-            child: Icon(
-              Icons.exit_to_app,
-              color: Colors.white,
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(200, 70),
+              backgroundColor: Colors.red,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: const BorderSide(color: Colors.white, width: 2),
+              ),
             ),
-            backgroundColor: Colors.pink),
-      ),
-    );
+            child: const Icon(
+              Icons.exit_to_app_outlined,
+              color: Colors.white,
+              size: 40,
+            ),
+          ),
+        ));
   }
 }
